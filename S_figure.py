043c684a3bdd50e490c6a3838cd8e3a1c29c8dbe
Kirill <36366596+justkkozlow@users.noreply.github.com>
2022-09-07 11:29:09 +1000

    Update S_figure.py

--- S_figure.py
+++ S_figure.py
@@ -1,3 +1,18 @@
+'''
+Описание задачи/Task description
+RU
+Жители страны Малевии часто экспериментируют с планировкой комнат. 
+Комнаты бывают треугольные, прямоугольные и круглые. 
+Чтобы быстро вычислять жилплощадь, требуется написать программу, на вход которой подаётся тип фигуры комнаты и соответствующие параметры, которая бы выводила площадь получившейся комнаты.
+Для числа π в стране Малевии используют значение 3.14.
+
+ENG
+Residents of Malevia often experiment with the layouts of their rooms. 
+Rooms are triangular, rectangular and round.
+To quickly calculate the living space, you need to write a program that receives the type of room figure and the corresponding parameters as input, which would display the area of the resulting room
+For the number π in the country of Malevia, the value 3.14 is used.
+'''
+
 f = input()
 
 if f == 'треугольник':
